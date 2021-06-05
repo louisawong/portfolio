e255a36a211427d0e0daf154858c63be11409640
Louisa <louisawong95@hotmail.com>
2021-06-05 13:44:42 -0400

    Added name and text animation

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,58 @@ import {useEffect} from 'react'
 export default function Home() {
 
   useEffect(()=>{
-    console.log(window.innerWidth)
+    descriptionAnimation();
   }
   ,[])
 
+  function descriptionAnimation () {
+    const contentList = [
+      "A Full Stack Engineer.",
+      "Coffee Lover ☕️.",
+      "Welcome to my space!"
+    ];
+
+    //type one text until text is finished
+    let text = document.getElementById("textAnimation")
+    let cursor = document.getElementById("cursor");
+    let sentenceIndex = 0;
+
+    function typing (sentence) {
+      if (sentence.length==1) {
+        let letter = sentence[0];
+        text.innerHTML+=(letter);
+        cursor.classList.add("cursorActive")
+        setTimeout(()=>deleteLetter(text.innerHTML),2000)
+        return;
+      }
+      cursor.classList.remove("cursorActive")
+      let letter = sentence[0];
+      text.innerHTML+=letter;
+      
+      setTimeout(()=>typing(sentence.slice(1)), 100);
+    }
+
+    function deleteLetter (sentence){
+      if (sentence.length==1) {
+        text.innerHTML= "";
+        cursor.classList.add("cursorActive")
+        if (sentenceIndex===contentList.length-1) {
+          sentenceIndex=0;
+        }
+        else {sentenceIndex++;}
+        setTimeout(()=>typing(contentList[sentenceIndex]), 2000);
+        return;
+      }
+      cursor.classList.remove("cursorActive")
+      let newSentence = text.innerHTML.substring(0,text.innerHTML.length-1)
+      text.innerText=newSentence;
+      setTimeout(()=>deleteLetter(newSentence), 100);
+    }
+
+    typing(contentList[0])
+
+  }
+  
   return (
     <div >
       <Head>
@@ -18,17 +66,22 @@ export default function Home() {
       <main className="main">
           
           <div className="hero">
-            <img src="/images/left_arrow.svg" className="leftarrow"/>
-            <div className="nameTitle">
-              <h1 className="fullname"> LOUISA WONG</h1>
+            <div className="hero_text">
+              <div className="hero_text_name">
+                <img src="/images/left_arrow.svg" className="arrow"/>
+                <h1 className="fullname"> LOUISA WONG</h1>
+                <img src="/images/right_arrow.svg" className="arrow" />
+              </div>
+              <div className="hero_text_description">
+                <h1 id="textAnimation"></h1>
+                <div id="cursor" className="cursor cursorActive">|</div>
+              </div>
             </div>
-            <img src="/images/right_arrow.svg" className="rightarrow" />
+            
+            <div className="hero_image">
+            </div>
           </div>
-            <h3 className="title" >Full Stack Developer</h3>
-        
-        <div className="projects">
-          Projects
-        </div>
+      
       </main>
       
     </div>
